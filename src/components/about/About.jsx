2f08cb5bae6a390886cc06a6bbb0e2b507ad4211
imamsudarajat04 <imamsudrajat707@gmail.com>
2022-05-26 16:40:47 +0700

    Update About Me Layouts

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import './About.css'
 import ME from '../../assets/me-about.jpg'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
 
 const About = () => {
   return (
@@ -11,11 +14,38 @@ const About = () => {
       <div className="container about__container">
         <div className="about__me">
           <div className="about__me-image">
-            <img src={ME} alt="" />
+            <img src={ME} alt="Its Me" />
           </div>
         </div>
 
         <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <FaAward className='about__icon' />
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
+
+            <article className='about__card'>
+              <FiUsers className='about__icon' />
+              <h5>Clients</h5>
+              <small>30+ Worldwide</small>
+            </article>
+
+            <article className='about__card'>
+              <VscFolderLibrary className='about__icon' />
+              <h5>Projects</h5>
+              <small>30+ Completed</small>
+            </article>
+          </div>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Dolores eum, qui aliquid laudantium commodi dolorem et odio in id ducimus vitae animi dolorum architecto, iusto, 
+            quasi totam quidem accusamus consectetur!
+          </p>
+
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
 
         </div>
       </div>
