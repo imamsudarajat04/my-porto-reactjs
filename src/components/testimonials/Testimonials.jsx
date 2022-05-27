e1b095882f44580ff42eda2241a1120d11dc6044
imamsudarajat04 <imamsudrajat707@gmail.com>
2022-05-27 08:57:29 +0700

    Update Testimonials Layouts

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,86 @@
 import React from 'react'
 import './Testimonials.css'
+import AVTR1 from '../../assets/avatar1.jpg'
+import AVTR2 from '../../assets/avatar2.jpg'
+import AVTR3 from '../../assets/avatar3.jpg'
+import AVTR4 from '../../assets/avatar4.jpg'
+
+// import Swiper core and required modules
+import { Pagination} from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
 
 const Testimonials = () => {
   return (
-    <section id='testimonials'>Testimonials</section>
+    <section id='testimonials'>
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+
+      <Swiper className="container testimonials__container"
+      modules={[Pagination]}
+      spaceBetween={40}
+      slidesPerView={1}
+      pagination={{ clickable: true }}
+      >
+        <SwiperSlide className="testimonial">
+          <div className="client__avatar">
+            <img src={AVTR1} alt="Avatar" />
+          </div>
+
+          <h5 className='client__name'>Nama</h5>
+            <small className='cleint__review'>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+              Saepe tenetur eius iure nostrum ea, maxime veritatis eaque ipsum doloremque voluptatem at porro aperiam, perferendis rerum. 
+              Facere autem recusandae asperiores adipisci.
+            </small>
+        </SwiperSlide>
+        
+        <SwiperSlide className="testimonial">
+          <div className="client__avatar">
+            <img src={AVTR2} alt="Avatar" />
+          </div>
+
+          <h5 className='client__name'>Nama</h5>
+            <small className='client__review'>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+              Saepe tenetur eius iure nostrum ea, maxime veritatis eaque ipsum doloremque voluptatem at porro aperiam, perferendis rerum. 
+              Facere autem recusandae asperiores adipisci.
+            </small>
+        </SwiperSlide>
+
+        <SwiperSlide className="testimonial">
+          <div className="client__avatar">
+            <img src={AVTR3} alt="Avatar" />
+          </div>
+
+          <h5 className='client__name'>Nama</h5>
+            <small className='client__review'>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+              Saepe tenetur eius iure nostrum ea, maxime veritatis eaque ipsum doloremque voluptatem at porro aperiam, perferendis rerum. 
+              Facere autem recusandae asperiores adipisci.
+            </small>
+        </SwiperSlide>
+
+        <SwiperSlide className="testimonial">
+          <div className="client__avatar">
+            <img src={AVTR4} alt="Avatar" />
+          </div>
+
+          <h5 className='client__name'>Nama</h5>
+            <small className='client__review'>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+              Saepe tenetur eius iure nostrum ea, maxime veritatis eaque ipsum doloremque voluptatem at porro aperiam, perferendis rerum. 
+              Facere autem recusandae asperiores adipisci.
+            </small>
+        </SwiperSlide>
+
+      </Swiper>
+    </section>
   )
 }
 
