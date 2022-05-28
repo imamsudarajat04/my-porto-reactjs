225c47d7f9a3010ca3caf75c4188294b3cf1d3e7
imamsudarajat04 <imamsudrajat707@gmail.com>
2022-05-28 10:27:34 +0700

    Update Layouts Contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,8 +3,20 @@ import './Contact.css'
 import {MdOutlineMail} from 'react-icons/md'
 import {RiMessengerLine} from 'react-icons/ri'
 import {BsWhatsapp} from 'react-icons/bs'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com'
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_0o62hiu', 'template_xcij7b6', form.current, 'CMHmom_i1Jxvc_uIk')
+      
+    e.target.reset();
+  };
+
   return (
     <section id='contact'>
       <h5>Get In Touch</h5>
@@ -13,28 +25,28 @@ const Contact = () => {
       <div className="container contact__container">
         <div className="contact__options">
           <article className='contact__option'>
-            <MdOutlineMail/>
+            <MdOutlineMail className='contact__option-icon'/>
             <h4>Email</h4>
             <h5>Imamsudrajat707@gmail.com</h5>
             <a href="mailto:imamsudrajat707@gmail.com" target='_blank'>Send a message</a>
           </article>
 
           <article className='contact__option'>
-            <RiMessengerLine/>
+            <RiMessengerLine className='contact__option-icon'/>
             <h4>Messenger</h4>
             <h5>Imam Sudarajat</h5>
             <a href="https://www.facebook.com/imamsudarajat04/" target='_blank'>Send a message</a>
           </article>
 
           <article className='contact__option'>
-            <BsWhatsapp/>
+            <BsWhatsapp className='contact__option-icon'/>
             <h4>WhatsApp</h4>
             <h5>+62 831 8346 2191</h5>
             <a href="https://api.whatsapp.com/send?phone=6283183462191" target='_blank'>Send a message</a>
           </article>
         </div>
 
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name="name" placeholder='Insert your name' required />
           <input type="email" name="email" placeholder='Insert your email' required />
           <textarea name="message" rows="7" placeholder='Insert your message'></textarea>
